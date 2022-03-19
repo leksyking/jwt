f0808d90495774ae716fba8b74c7bef924c5891a
Leksyking <gbemilekeogundipe@gmail.com>
2022-03-19 02:28:20 -0700

    initial commit

---
+++ errors/index.js
@@ -0,0 +1,10 @@
+const customAPIError = require('./custom-error')
+const BadRequest = require('./badRequest')
+const UnauthenticatedError = require('./unauthenticated')
+
+
+module.exports = {
+    customAPIError,
+    BadRequest,
+    UnauthenticatedError
+}
